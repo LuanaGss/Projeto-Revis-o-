987a5bf0ecb97c2be5e33db183a7da6037668cf2
LuanaGss <71847197+LuanaGss@users.noreply.github.com>
2025-03-21 20:04:17 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//Vamos criar um programa que gerencie uma **lista de notas de alunos** e calcule a média. O usuário poderá:
+//Vamos criar um programa que gerencie uma **lista de notas de alunos** e calcule a média. O usuário poderá:
 
 //1.Inserir as notas de um aluno. (Cada aluno tem 5 notas)
 //2. Exibir as notas e calcular a média.
@@ -14,34 +14,40 @@ for (int i = 0; i < notas.Length; i++)
     {
         Console.WriteLine("Digite a nota do aluno: ");
         notas[i] = int.Parse(Console.ReadLine());
+
+        if (notas[i] < 0 || notas[i] > 10)
+        {
+            throw new ArgumentOutOfRangeException("Digite uma nota entre 0 e 10");
+        }
     }
     catch (FormatException ex)
     {
         Console.WriteLine("Digite apenas numeros inteiros");
-        break;
     }
     catch (ArgumentOutOfRangeException ex)
     {
         Console.WriteLine(ex.ParamName);
-        break;
-    }
-    if (notas[i] >= 0 && notas[i] <= 10)
-    {
-        throw new ArgumentOutOfRangeException("Digite uma nota entre 0 e 10");
     }
 }
+
+Console.WriteLine("Notas do aluno:");
 
 foreach (int item in notas)
 {
-    Console.WriteLine(item);
+    Console.Write(item + " ");
     soma = item + soma;
 }
 
+Console.WriteLine("");
+
+Console.WriteLine($"Média: {soma / notas.Length}");
+
 if (soma / notas.Length >= 7)
 {
-    Console.WriteLine("Aluno aprovado");
+    Console.WriteLine("O aluno foi APROVADO!");
 }
 else
 {
-    Console.WriteLine("Aluno reprovado");
+    Console.WriteLine("O aluno foi reprovado");
 }
+
